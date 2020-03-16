b4b8df3d7e4fbd484da23e204ca4fe9a729c791f
Human Fear <pnam5005@gmail.com>
2020-03-16 20:22:43 +0700

    Update enchantment support PiggyCustomEnchants!

--- src/hachkingtohach1/CustomCraft/Main.php
+++ src/hachkingtohach1/CustomCraft/Main.php
@@ -5,44 +5,93 @@ namespace hachkingtohach1\CustomCraft;
 use pocketmine\item\Item;
 use pocketmine\plugin\PluginBase;
 use pocketmine\inventory\ShapedRecipe;
-use pocketmine\item\enchantment\Enchantment;
-use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\enchantment\{Enchantment,EnchantmentInstance};
+use DaPigGuy\PiggyCustomEnchants\{CustomEnchantManager,PiggyCustomEnchants,utils\Utils};
 
 class Main extends PluginBase { 
 
-    public function onLoad() : void {
+    public function onLoad() : void 
+	{
         $this->saveDefaultConfig();
     }	
 
-    public function onEnable() : void {
+    public function onEnable() : void 
+	{
+		$this->checkPluginNeed();
         $this->registerItemsCraft();		
-    }     	
+    }
+
+    public function checkPluginNeed() 
+	{
+		$this->ce = $this->getServer()->getPluginManager()->getPlugin('PiggyCustomEnchants');
+        if($this->ce === null) {
+			$this->getLogger()->warning('You need install plugin PiggyCustomEnchants to use this plugin!');
+            $this->getServer()->shutDown();				
+		}
+	}		
     
 	/**
 	 *  This is getEnchantment int $id
 	 **/
-    public function getEnchantment(int $id) {		
+    public function getEnchantment(int $id) 
+	{	
 		$enchantment = Enchantment::getEnchantment($id);	
 		return $enchantment;
-	}		
+	}
     
-    public function getItem(array $item) : Item {
-		
+	/**
+	 * This is getCEnchantment string $name Item $item, int $level
+	 **/
+    public function getCEnchantment(string $name, $item, int $level) 
+	{
+		$items = Item::fromString($item);
+		$enchant = CustomEnchantManager::getEnchantmentByName($name);
+        if ($enchant === null) {
+			$this->getLogger()->warning('CE is '.$name.' with level'.$level.' name is null!');
+			return;
+		}
+		if ($level > $enchant->getMaxLevel()) {
+			$this->getLogger()->warning('CE is '.$name.' with level'.$level.' max level is '.$enchant->getMaxLevel());
+			return;
+		}
+		if(!Utils::checkEnchantIncompatibilities($items, $enchant)) {
+			$this->getLogger()->warning('CE is '.$name.' with level '.$level.'This enchant is not compatible with another enchant.');
+            return;
+        }
+		return $enchant;		
+	}
+
+    public function getItem(array $item) : Item 
+	{		
         $items = Item::fromString($item[0]);	
         if(isset($item[1])) { 
 		    $items->setCount((int) $item[1]);
-			if(!$item[2] == "0" || !$item[3] == "0") {
-			    $items->addEnchantment(new EnchantmentInstance($this->getEnchantment($item[2]), $item[3]));
+        } 
+        return $items;
+    }	
+    
+    public function getItemResult(array $item, array $enchantment, array $cenchantment) : Item 
+	{	
+        $items = Item::fromString($item[0]);
+        $ec = $enchantment;
+        $ce = $cenchantment; 		
+        if(isset($item[1])) { 
+		    $items->setCount((int) $item[1]);
+			if(!$enchantment[0] == "0" && !$enchantment[1] == "0") {
+			    $items->addEnchantment(new EnchantmentInstance($this->getEnchantment($ec[0]), $ec[1]));
+			}
+			if(!$ce[0] == "0" && !$ce[1] == "0" && !$ce[2] == "0") {
+				$items->addEnchantment(new EnchantmentInstance($this->getCEnchantment($ce[0], $ce[1], $ce[2]), $ce[2]));
 			}
         } 
         return $items;
     }
 	
 	// Lenght => array() & short => []	
-	public function registerItemsCraft() {
-		
-		foreach($this->getConfig()->getAll() as $craft) {
-		
+	public function registerItemsCraft() 
+	{		
+		foreach($this->getConfig()->getAll() as $craft) 
+		{		
             $recipes = new ShapedRecipe( 
 			array("abc","def","ghi"), 
 			    array (			
@@ -56,7 +105,7 @@ class Main extends PluginBase {
 		            "h" => $this->getItem($craft["shape"][2][1]),
 		            "i" => $this->getItem($craft["shape"][2][2]), 
 			    ), 
-			    [$this->getItem($craft["result"])]
+			    [$this->getItemResult($craft["result"], $craft["enchantment"], $craft["cenchantment"])]
 			);				
             $this->getServer()->getCraftingManager()->registerRecipe($recipes);
         }
